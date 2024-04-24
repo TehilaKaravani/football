8f2d6705fb7a7b6ff4a2af2adadf91ddb4d6a526
DarinaKir <dasha23kir@gmail.com>
2024-04-24 16:39:03 +0300

    24.04.2024

--- src/DashboardPage.jsx
+++ src/DashboardPage.jsx
@@ -1,84 +1,47 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const DashboardPage = () => {
     const [cycle, setCycle] = useState([]);
 
-
     useEffect(() => {
-        const event = new EventSource("http://localhost:9124/start-streaming");
-        event.onopen = function () {
-            console.log("connection is opened " + event.readyState);
+        const eventSource = new EventSource("http://localhost:9124/start-streaming"); // התחברות לנתיב שבו מופעל ה־SSE
+
+        eventSource.onmessage = event => {
+            const data = JSON.parse(event.data);
+            setCycle(data); // עדכון המצב עם המערך של המשחקים שנשלח מהשרת
         };
 
-        event.onmessage = function (messages) {
-            // const newData = JSON.parse(messages.data);
-            // setCycle(prevData => [...prevData, newData]);
-            setCycle(messages.data)
-        }
+        return () => {
+            eventSource.close(); // סגירת החיבור כאשר הרכיב מוסר
+        };
     }, []);
-
-    // useEffect(() => {
-    //     console.log(cycle);
-    // }, [cycle]);
-
 
     return (
         <div className='container'>
-
-
+            <h1>Matches</h1>
             <table>
                 <thead>
                 <tr>
-                    <th>
-                        Team1
-                    </th>
-                    <th>
-                        Team2
-                    </th>
-                    <th>
-                        Goal1
-                    </th>
-                    <th>
-                        Goal2
-                    </th>
+                    <th>Team1</th>
+                    <th>Team2</th>
+                    <th>Goal1</th>
+                    <th>Goal2</th>
                 </tr>
                 </thead>
-
-
-                {/*{cycle.map((item, index) => (*/}
-                {/*    <div key={index}>*/}
-                {/*        <p>{item}</p>*/}
-                {/*    </div>*/}
-                {/*))}*/}
-
-
-                {/*<tbody>*/}
-                {/*{*/}
-                {/*    cycle.map((match, index) => {*/}
-                {/*        return (*/}
-                {/*            <tr key={index}>*/}
-                {/*                <td>*/}
-                {/*                    {match.team1.name}*/}
-                {/*                </td>*/}
-                {/*                <td>*/}
-                {/*                    {match.team2.name}*/}
-                {/*                </td>*/}
-                {/*                <td>*/}
-                {/*                    {match.goals_T1}*/}
-                {/*                </td>*/}
-                {/*                <td>*/}
-                {/*                    {match.goals_T2}*/}
-                {/*                </td>*/}
-                {/*            </tr>*/}
-                {/*        )*/}
-                {/*    })*/}
-                {/*}*/}
-                {/*</tbody>*/}
+                <tbody>
+                {cycle.map((match, index) => (
+                    <tr key={index}>
+                        <td>{match.team1.name}</td>
+                        <td>{match.team2.name}</td>
+                        <td>{match.goals_T1}</td>
+                        <td>{match.goals_T2}</td>
+                    </tr>
+                ))}
+                </tbody>
             </table>
-            {cycle}
         </div>
-    )
-}
+    );
+};
 
 export default DashboardPage;
